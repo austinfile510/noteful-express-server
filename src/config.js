79ea90566898d4e00a81557b3812bf96f037c5d3
Austin File <austinfile510@gmail.com>
2021-01-20 19:13:56 -0800

    updated package.json

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-	DB_URL:
-		process.env.DB_URL || 'postgresql://postgres:wetbeverage@localhost/noteful',
-	TEST_DB_URL:
-		process.env.DB_URL ||
+	DATABASE_URL:
+		process.env.DATABASE_URL || 'postgresql://postgres:wetbeverage@localhost/noteful',
+	TEST_DATABASE_URL:
+		process.env.DATABASE_URL ||
 		'postgresql://postgres:wetbeverage@localhost/noteful-test',
 };
