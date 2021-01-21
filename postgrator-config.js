968d985ea4dcb5e5353e275bf43ef6dce7fa8957
Austin File <austinfile510@gmail.com>
2021-01-20 19:20:51 -0800

    updated postgrator-config.js

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-	migrationDirectory: 'migrations',
-	driver: 'pg',
+	"migrationDirectory": 'migrations',
+	"driver": 'pg',
 	connectionString:
 		process.env.NODE_ENV === 'test'
 			? process.env.TEST_DATABASE_URL
